47b0d8c109544306f8402cb769bbddbe2c0fcae1
Anjana Vakil <contact@anjana.dev>
2025-11-11 19:52:27 -0500

    mvp working queue! and cleanup

--- lib/karaoke_web/live/song_live/index.ex
+++ lib/karaoke_web/live/song_live/index.ex
@@ -2,7 +2,7 @@ defmodule KaraokeWeb.SongLive.Index do
   use KaraokeWeb, :live_view
 
   alias Karaoke.Songs
-  require Logger
+
 
   @impl true
   def render(assigns) do
@@ -12,49 +12,33 @@ defmodule KaraokeWeb.SongLive.Index do
         Karaoke Songs!
       </.header>
 
-      <.form for={@form} id="song-form-new" phx-submit="save" class="sm:flex sm:align-center sm:gap-2">
-        <div class="flex-grow">
-          <.input field={@form[:title]} type="text" label="Title" />
-        </div>
-        <.input field={@form[:singer]} type="text" label="Singer" />
-        <div class="h-full mt-[26px]">
-          <.button phx-disable-with="Saving..." variant="primary">
-            <.icon name="hero-plus" /> Add Song
-          </.button>
-        </div>
-      </.form>
-
-      <div id="songs" phx-update="stream">
-        <.live_component
-          :for={{id, song} <- @streams.songs}
-          song={song}
-          module={KaraokeWeb.QueuedSongLive}
-          id={song.id}
-        />
+      <div class="font-bold grid grid-cols-3 gap-2">
+        <span>SONG</span>
+        <span>SINGER</span>
+        <span></span>
       </div>
 
-      <.table
-        id="songs"
-        rows={@streams.songs}
-      >
-        <:col :let={{_id, song}} label="Title">
-          {song.title}
-        </:col>
-        <:col :let={{_id, song}} label="Singer">
-          {song.singer}
-        </:col>
-        <:action :let={{_id, song}}>
-          <.link phx-click="toggle_edit" phx-value-id={song.id}>Edit</.link>
-        </:action>
-        <:action :let={{id, song}}>
-          <.link
-            phx-click={JS.push("delete", value: %{id: song.id}) |> hide("##{id}")}
-            data-confirm="Are you sure?"
-          >
-            Delete
-          </.link>
-        </:action>
-      </.table>
+      <div id="queued_songs" phx-update="stream" >
+        <.live_component
+          :for={{id, song} <- @streams.songs}
+          :key={id}
+          module={KaraokeWeb.QueuedSongLive}
+          id={id}
+          song={song}
+          editing={false}
+          />
+      </div>
+
+       <.form for={@form} id="song-form-new" phx-submit="new" class="grid grid-cols-3 gap-2">
+          <.input field={@form[:title]} type="text" placeholder="song title" />
+        <.input field={@form[:singer]} type="text" placeholder="singer name" />
+          <.button phx-disable-with="Saving..." variant="primary">
+          <.icon name="hero-plus" /> Add Song</.button>
+      </.form>
+
+
+
+
     </Layouts.app>
     """
   end
@@ -65,19 +49,29 @@ defmodule KaraokeWeb.SongLive.Index do
      socket
      |> assign(:page_title, "Listing Songs")
      |> assign(form: to_form(%{"title" => "", "singer" => ""}, action: :save))
-     |> stream(:songs, [])}
+     |> stream(:songs, [%Songs.Song{title: "Closing Time", singer: "Everyone", id: "Everyone-Closing Time"}])}
+  end
+
+
+  @impl true
+  def handle_info({:save, data}, socket) do
+    {:noreply, stream_insert(socket, :songs, data)}
   end
 
   @impl true
-  def handle_event("save", %{"title" => title, "singer" => singer}, socket) do
+  def handle_event("new", %{"title" => title, "singer" => singer}, socket) do
     this_song_id = "#{singer}-#{title}"
     new_song = %Songs.Song{title: title, singer: singer, id: this_song_id}
-    {:noreply, stream_insert(socket, :songs, new_song)}
+    {:noreply, socket
+      |> assign(form: to_form(%{"title" => "", "singer" => ""}, action: :save))
+      |> stream_insert(:songs, new_song)
+    }
   end
 
   @impl true
-  def handle_event("edit", %{"id" => id}, socket) do
-    Logger.info("hello")
+  def handle_event("toggle_edit", %{"id" => id}, socket) do
+    dbg("toggle_edit")
+    {:noreply, socket}
   end
 
   @impl true
@@ -86,6 +80,4 @@ defmodule KaraokeWeb.SongLive.Index do
     {:noreply, stream_delete_by_dom_id(socket, :songs, dom_id)}
   end
 
-  def add_song() do
-  end
 end
---
+++ lib/karaoke_web/live/song_live/queued_song.ex
@@ -0,0 +1,62 @@
+defmodule KaraokeWeb.QueuedSongLive do
+  use KaraokeWeb, :live_component
+
+  @impl true
+  def render(assigns) do
+    editing = assigns.editing
+    if editing do
+    ~H"""
+    <div id={@song.id} class="flex">
+    <.form for={@form} id="edit-song-form" phx-target={@myself} phx-submit="save" class="grid grid-cols-3 gap-2 py-2">
+          <%!-- <span>{@song.id}</span> --%>
+          <.input field={@form[:title]} id={@song.id <> "-title"} type="text" />
+          <.input field={@form[:singer]} id={@song.id <> "-singer"} type="text" />
+          <.button>Save</.button>
+      </.form>
+    </div>
+    """
+    else
+      ~H"""
+      <div id={@song.id} class="grid grid-cols-3 gap-2 py-2">
+        <%!-- <span>{@song.id}</span> --%>
+        <span>{@song.title}</span>
+        <span>{@song.singer}</span>
+        <%!-- <span>Editing? {@editing}</span> --%>
+        <.button phx-click="edit" phx-target={@myself}>Edit</.button>
+
+      </div>
+      """
+
+    end
+
+  end
+
+  @impl true
+  def handle_event("edit", _params, socket) do
+    song = socket.assigns.song
+    {:noreply, socket
+      |> assign(form: to_form(%{"title" => song.title, "singer" => song.singer, "id" => song.id}, action: :save ))
+      |> assign(editing: true)
+    }
+  end
+
+  @impl true
+  def handle_event("save", %{"title" => title, "singer" => singer}, socket) do
+    data = %{title: title, singer: singer, id: socket.assigns.song.id}
+    send(self(), {:save, data})
+    {:noreply, socket
+      |> assign(editing: false)
+    }
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    assigns |> dbg
+    {:ok, assign(socket, assigns)}
+  end
+
+  # @impl true
+  # def mount(socket) do
+  #   {:ok, socket |> assign(socket.assigns)}
+  # end
+end
