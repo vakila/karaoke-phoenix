d2da917168e1888429c9986c971cf56b0222394e
Anjana Vakil <contact@anjana.dev>
2025-11-25 14:33:18 -0500

    update config

--- config/dev.exs
+++ config/dev.exs
@@ -8,8 +8,9 @@ import Config
 # to bundle .js and .css sources.
 config :karaoke, KaraokeWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
+  # originally `ip: {127, 0, 0, 1}`
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
